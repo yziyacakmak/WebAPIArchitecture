5405c4c48d538781e713f6d6ca7d7d1cd91b849f
yusuf.cakmak <yusuf.cakmak@saykal.com>
2025-02-18 15:56:53 +0300

    repository migration

--- App.API/Program.cs
+++ App.API/Program.cs
@@ -1,3 +1,7 @@
+using App.Repositories;
+using App.Repositories.Extension;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddRepository(builder.Configuration);
+
+
+
 
 var app = builder.Build();
 
---
+++ Repositories/Extension/RepositoryExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace App.Repositories.Extension
+{
+    public static class RepositoryExtensions
+    {
+        public static IServiceCollection AddRepository(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                var connectionStrings = configuration
+                .GetSection(ConnectionStringOption.Key)
+                .Get<ConnectionStringOption>();
+                options.UseSqlServer(connectionStrings!.SqlServer,sqlServerOptionsAction=>
+                { 
+                    sqlServerOptionsAction.MigrationsAssembly(typeof(RepositoryAssembly).Assembly.FullName);
+                });
+            });
+            return services;
+        }
+    }
+}
